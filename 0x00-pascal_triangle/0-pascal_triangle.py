65defb4464b1e13ee91d69e4a0b6da4f34b7c6fa
MohamedH1000 <mohammedhisham115@gmail.com>
2023-11-30 17:08:35 +0000

    0. Pascal's Triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+'''find pascal triangle integer by this module'''
+
+
+def pascal_triangle(n):
+    '''
+    pascal triangle integers to be found by this function
+    '''
+    pascal_triangle = list()
+
+    if n <= 0:
+        return pascal_triangle
+
+    if n > 0:
+        pascal_triangle.append([1])
+
+    if n > 1:
+        pascal_triangle.append([1, 1])
+
+    for x in range(3, n+1):
+        pascal_triangle.append([0] * x)
+
+        pascal_triangle[x-1][0] = 1
+        pascal_triangle[x-1][x-1] = 1
+
+        for y in range(1, x-1):
+            pascal_triangle[x-1][y] = \
+                pascal_triangle[x-2][y-1] + pascal_triangle[x-2][y]
+
+    return pascal_triangle
