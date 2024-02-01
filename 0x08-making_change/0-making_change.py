c319dc1cae1b83c9336a782007b3dfbd519cea64
MohamedH1000 <mohammedhisham115@gmail.com>
2024-02-01 06:51:30 -0800

    making change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-""" A function to determine 
+""" A function to determine
     the fewest number of coins needed
     to meet a given amount total"""
 
 
 def makeChange(coins, total):
-    """This function will 
+    """This function will
         take a list of coins and use
-       that to calculate 
-       how much change the 
+       that to calculate
+       how much change the
        total will require
     """
     if total <= 0:
